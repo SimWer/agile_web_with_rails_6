a4f262e74019b087e91422f422036bf013f68c83
Ove Simon Wernersson <simon.wernersson@live.no>
2020-01-10 13:59:20 +0100

    add shopping cart with basic functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
+  # . . .
+
   private
   # ensure that there are no line items referencing this product
   def ensure_not_referenced_by_any_line_item
