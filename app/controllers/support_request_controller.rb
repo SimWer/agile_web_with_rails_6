437f8ec66c3f292038b2dbe4d09cfda1b353caa0
Ove Simon Wernersson <simon.wernersson@live.no>
2020-01-21 08:58:48 +0100

    Add support mail and response mailer

--- app/controllers/support_request_controller.rb
+++ app/controllers/support_request_controller.rb
@@ -4,5 +4,9 @@ class SupportRequestController < ApplicationController
   end
 
   def update
+    support_request = SupportRequest.find(params[:id])
+    support_request.update(response: params.require(:support_request)[:response])
+    SupportRequestMailer.respond(support_request).deliver_now
+    redirect_to support_request_index_path
   end
 end
